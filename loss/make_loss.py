ffda15cce9558329b88f5e5deac357e932df6f80
Shan Lin <LinShanify@gmail.com>
2019-03-11 18:35:40 +0800

    reverse imp triplet

--- loss/make_loss.py
+++ loss/make_loss.py
@@ -5,11 +5,13 @@ import torch.nn.functional as F
 
 from .triplet_loss import TripletLoss
 from .imptriplet_loss import ImpTripletLoss
+from .retriplet_loss import ReTripletLoss
 
 def make_loss(cfg):
     sampler = cfg.DATALOADER.SAMPLER
     triplet = TripletLoss(cfg.SOLVER.MARGIN)  # triplet loss
     imptriplet = ImpTripletLoss(cfg.SOLVER.MARGIN)  # triplet loss
+    retriplet = ReTripletLoss(cfg.SOLVER.MARGIN)  # triplet loss
 
     if sampler == 'softmax':
         def loss_func(score, feat, target):
@@ -23,6 +25,9 @@ def make_loss(cfg):
     elif sampler == 'softmax_imptriplet':
         def loss_func(score, feat, target):
             return F.cross_entropy(score, target) + imptriplet(feat, target)[0]
+    elif sampler == 'softmax_retriplet':
+        def loss_func(score, feat, target):
+            return F.cross_entropy(score, target) + retriplet(feat, target)[0]
     else:
         print('expected sampler should be softmax, triplet, softmax_triplet or softmax_imptriplet, '
               'but got {}'.format(sampler))
